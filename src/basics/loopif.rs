34e1ec00f21c6a42d7e2b5073fda7745d49bdad4
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-15 16:03:36 +0100

    Learn for-loop with conditionals

--- src/basics/loopif.rs
+++ src/basics/loopif.rs
@@ -1,5 +1,6 @@
 //! Learn about loops and ifs
 
+// for loop
 mod for1 {
     pub fn for1_example() {
         for i in 0..5 {
@@ -8,6 +9,20 @@ mod for1 {
     }
 }
 
+// for loop with conditional
+mod for2 {
+    pub fn for2_example() {
+        for i in 0..5 {
+            if i % 2 == 0 {
+                println!("even {}", i);
+            } else {
+                println!("odd {}", i);
+            }
+        }
+    }
+}
+
 pub fn loop_and_if_example() {
     for1::for1_example();
+    for2::for2_example();
 }
