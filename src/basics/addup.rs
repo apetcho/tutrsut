dac746b6bea1075b89903f96ecbba988ad48b9c4
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-15 16:29:25 +0100

    Learn about type casting

--- src/basics/addup.rs
+++ src/basics/addup.rs
@@ -10,6 +10,18 @@ mod add12 {
     }
 }
 
+/// type cast example
+mod add3 {
+    pub fn add3_example() {
+        let mut sum = 0.0;
+        for i in 0..5 {
+            sum += i as f64;
+        }
+        println!("sum is {}", sum);
+    }
+}
+
 pub fn addup_example() {
     add12::add12_example();
+    add3::add3_example();
 }
