4940b366ec830652e26c9cddf79fd7413d45b438
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-15 21:01:19 +0100

    Learn std::env::args, nth, expect, and parse

--- src/basics/cmdargs.rs
+++ src/basics/cmdargs.rs
@@ -1,13 +1,25 @@
 //! Getting Command Line Arguments
 
-mod arg0 {
-    pub fn arg0_examples() {
+mod args0 {
+    pub fn args0_examples() {
         for arg in std::env::args() {
             println!("'{}'", arg);
         }
     }
 }
 
+// nth() and expect()
+mod args1 {
+    use std::env;
+
+    pub fn args1_examples() {
+        let first = env::args().nth(1).expect("please supply an argument");
+        let n: i32 = first.parse().expect("not an integer");
+        println!("argv[1] = {}", n);
+    }
+}
+
 pub fn cmdargs_examples() {
-    arg0::arg0_examples();
+    args0::args0_examples();
+    args1::args1_examples();
 }
