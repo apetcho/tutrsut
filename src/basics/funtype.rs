22b5ea579ea15e45c20e3790eca051680b72f4a0
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-15 17:02:25 +0100

    Learn about *pass by reference* concept in rust

--- src/basics/funtype.rs
+++ src/basics/funtype.rs
@@ -11,6 +11,58 @@ mod fun1 {
     }
 }
 
+/// More user-defined functions
+mod fun2 {
+    // Passy by reference
+    fn by_ref(x: &i32) -> i32 {
+        *x + 1
+    }
+
+    pub fn fun2_examples() {
+        let absval = abs(-2.0);
+        assert_eq!(absval, 2.0);
+        assert_eq!(clamp(-1.0, 0.0, 1.0), 0.0);
+        assert_eq!(factorial(3), 6);
+
+        let i = 10;
+        let res1 = by_ref(&i);
+        let res2 = by_ref(&42);
+        println!("{} {}", res1, res2);
+    }
+
+    // absolute value of a floating-point number
+    fn abs(x: f64) -> f64 {
+        if x > 0.0 {
+            x
+        } else {
+            -x
+        }
+    }
+
+    // ensure the number always falls in the given range
+    fn clamp(x: f64, x1: f64, x2: f64) -> f64 {
+        if x < x1 {
+            x1
+        } else if x > x2 {
+            x2
+        } else {
+            x
+        }
+    }
+
+    // recursive factorial
+    fn factorial(n: u64) -> u64 {
+        if n == 0 {
+            1
+        } else {
+            n * factorial(n - 1)
+        }
+    }
+}
+
+// entry point
 pub fn fun_examples() {
     fun1::fun1_example();
+    println!("----- Pass by reference -----");
+    fun2::fun2_examples();
 }
