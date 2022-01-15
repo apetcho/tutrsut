028af1a6ad1c26bf881bbfe664f5260018123307
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-15 17:36:35 +0100

    Learn to pass slice to a function

--- src/basics/arrslice.rs
+++ src/basics/arrslice.rs
@@ -14,6 +14,24 @@ mod array1 {
     }
 }
 
+// Pass array or slice to a function
+mod array2 {
+    fn sum(values: &[i32]) -> i32 {
+        let mut res = 0;
+        for i in 0..values.len() {
+            res += values[i];
+        }
+        res
+    }
+    pub fn array2_example() {
+        let arr = [10, 20, 30];
+        println!("arr = {:?}", &arr);
+        let res = sum(&arr);
+        println!("sum = {}", res);
+    }
+}
+
 pub fn arrslice_example() {
     array1::array1_example();
+    array2::array2_example();
 }
