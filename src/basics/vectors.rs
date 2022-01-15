3410660cdbe1ead10edf17ffc8a1fe7d64075672
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-15 18:34:01 +0100

    Learn relationship between vectors and slices

--- src/basics/vectors.rs
+++ src/basics/vectors.rs
@@ -15,6 +15,23 @@ mod vec1 {
     }
 }
 
+// relationship between vectors and slices
+mod vec2 {
+    fn dump(arr: &[i32]) {
+        println!("arr is {:?}", arr);
+    }
+    pub fn vec2_example() {
+        let mut v = Vec::new();
+        v.push(10);
+        v.push(20);
+        v.push(30);
+        dump(&v);
+        let slice = &v[1..];
+        println!("slice is {:?}", slice);
+    }
+}
+
 pub fn vectors_examples() {
     vec1::vec1_example();
+    vec2::vec2_example();
 }
