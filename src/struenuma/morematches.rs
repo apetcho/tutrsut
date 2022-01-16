be9ef77b654342612e22de616e0b744bffa40061
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-16 12:52:39 +0100

    Learn how to destructure a Struct

--- src/struenuma/morematches.rs
+++ src/struenuma/morematches.rs
@@ -4,9 +4,17 @@ mod mam {
     pub fn mam_examples() {
         let t = (10, "Rust".to_string());
         let (ref n, ref s) = t;
-        println!("=> n = {}, s = {}", n, s);
+        println!("=> n={}, s={}", n, s);
         let (n, s) = t;
-        println!("=> n = {}, s = {}", n, s);
+        println!("=> n={}, s={}", n, s);
+        // -- Structurin struct
+        struct Point {
+            x: f32,
+            y: f32,
+        }
+        let p = Point { x: 1.0, y: 2.0 };
+        let Point { x, y } = p;
+        println!("=> x={}, y={}", x, y);
     }
 }
 
