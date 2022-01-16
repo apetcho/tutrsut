376ebe4052bbeb92b3e9c8d08199652abb33ec52
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-16 12:59:32 +0100

    Borrow String from tuple with no destructive destructuring

--- src/struenuma/morematches.rs
+++ src/struenuma/morematches.rs
@@ -1,6 +1,18 @@
 //! More about matching
 
 mod mam {
+    // --
+    fn match_tuple(t: (i32, String)) {
+        let text = match t {
+            (0, s) => format!("zero {}", s),
+            (1, ref s) if s == "hello" => format!("hello one!"),
+            tt => format!("no match {:?}", tt),
+        };
+
+        println!("{}", text);
+    }
+
+    // --
     pub fn mam_examples() {
         let t = (10, "Rust".to_string());
         let (ref n, ref s) = t;
@@ -15,6 +27,13 @@ mod mam {
         let p = Point { x: 1.0, y: 2.0 };
         let Point { x, y } = p;
         println!("=> x={}, y={}", x, y);
+        // --
+        match_tuple((0, "None".to_string()));
+        // --
+        let ot = Some((2, "hello".to_string()));
+        if let Some((_, ref s)) = ot {
+            assert_eq!(s, "hello");
+        }
     }
 }
 
