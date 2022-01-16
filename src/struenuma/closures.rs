e8ee3cfe59496ac0e1fd8370505677f6cf34be6f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-16 15:22:29 +0100

    Learn how to pass closure as a function argument

--- src/struenuma/closures.rs
+++ src/struenuma/closures.rs
@@ -1,6 +1,14 @@
 //! Closures
 
 mod closurestut {
+    fn apply<F>(x: f64, f: F) -> f64
+    where
+        F: Fn(f64) -> f64,
+    {
+        f(x)
+    }
+
+    // --
     pub fn closurestut_examples() {
         let f = |x| x * x;
         let res = f(10);
@@ -10,6 +18,9 @@ mod closurestut {
         let c = 1.0;
         let lin = |x| m * x + c;
         println!("reslin: {} {}", lin(1.0), lin(2.0));
+        let res1 = apply(3.0, lin);
+        let res2 = apply(3.14, |x| x.sin());
+        println!("res1={}, res2={}", res1, res2);
     }
 }
 
