8e9dc96a124a0219cc9d8bf10bf422780607d26c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-16 10:53:47 +0100

    Learn how to store value in enums

--- src/struenuma/enums.rs
+++ src/struenuma/enums.rs
@@ -1,6 +1,6 @@
 //! Simple Enums
 
-mod enum2 {
+mod enum12 {
     enum Speed {
         #[allow(dead_code)]
         Slow = 10,
@@ -11,13 +11,33 @@ mod enum2 {
     }
 
     // ---
-    pub fn enum2_examples() {
+    pub fn enum12_examples() {
         let s = Speed::Slow;
         let speed = s as u32;
         println!("speed = {}", speed);
     }
 }
 
+// --
+mod enum3 {
+    #[derive(Debug)]
+    enum Value {
+        Number(f64),
+        Str(String),
+        Bool(bool),
+    }
+
+    pub fn enum3_examples() {
+        use Value::*;
+        let n = Number(2.3);
+        let s = Str("Hello".to_string());
+        let b = Bool(true);
+
+        println!("n = {:?}, s = {:?}, b = {:?}", n, s, b);
+    }
+}
+
 pub fn enums_examples() {
-    enum2::enum2_examples();
+    enum12::enum12_examples();
+    enum3::enum3_examples();
 }
