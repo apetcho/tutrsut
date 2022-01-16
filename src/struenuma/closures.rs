3671787a38915264d7770aa3c9f1de624b464abd
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-16 15:18:45 +0100

    Learn to borrow (capture) context variable in closures

--- src/struenuma/closures.rs
+++ src/struenuma/closures.rs
@@ -5,6 +5,11 @@ mod closurestut {
         let f = |x| x * x;
         let res = f(10);
         println!("res={}", res);
+        // let resf = f(1.2);  // #Error:: mismatch type
+        let m = 2.0;
+        let c = 1.0;
+        let lin = |x| m * x + c;
+        println!("reslin: {} {}", lin(1.0), lin(2.0));
     }
 }
 
