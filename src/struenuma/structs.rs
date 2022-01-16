117d5063094e8cd0db1d3c225eb170c47e0f1307
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-16 04:16:10 +0100

    Learn about impl and Associated Functions

--- src/struenuma/structs.rs
+++ src/struenuma/structs.rs
@@ -15,6 +15,30 @@ mod struct1 {
     }
 }
 
+// Associated function and impl
+mod struct2 {
+    struct Person {
+        first_name: String,
+        last_name: String,
+    }
+    // impl
+    impl Person {
+        fn new(first: &str, last: &str) -> Person {
+            Person {
+                first_name: first.to_string(),
+                last_name: last.to_string(),
+            }
+        }
+    }
+
+    // -- using Person
+    pub fn struct2_examples() {
+        let p = Person::new("John", "Smith");
+        println!("person: {} {}", p.first_name, p.last_name);
+    }
+}
+
 pub fn structs_examples() {
     struct1::struct1_examples();
+    struct2::struct2_examples();
 }
