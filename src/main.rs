4cb02d79b4f8f568259ae7446876674e1b539083
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-16 03:15:31 +0100

    Introduce move semantic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-mod basics;
+//mod basics;
 mod struenuma;
 
 // -----------
 // MAIN DRIVER
 // -----------
 fn main() {
-    println!("============");
-    println!("   BASICS   ");
-    println!("============");
-    basics::basictut::basictut_examples();
+    //println!("============");
+    //println!("   BASICS   ");
+    //println!("============");
+    //basics::basictut::basictut_examples();
+    println!("===========================");
+    println!("   STRUCTS, ENUMS, MATCH   ");
+    println!("===========================");
     struenuma::tutorial::structs_enums_and_matching();
 }
--- src/struenuma/moveit.rs
+++ src/struenuma/moveit.rs
@@ -1,7 +1,15 @@
 //! Rust like to Move It, Move It
 
 mod movetut {
-    pub fn movetut_examples() {}
+    fn dump(s: &str) {
+        println!("{}", s);
+    }
+    pub fn movetut_examples() {
+        let s1 = "Hello World!".to_string();
+        //let s2 = s1;
+        dump(&s1);
+        println!("s1 = {}", s1);
+    }
 }
 
 pub fn moveit_examples() {
