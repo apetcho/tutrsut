b78bcd6b60dd8cdfde60395ee5bfa2d7849ea1c9
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-17 18:59:10 +0100

    Learn how to run shell command as a separate process

--- src/fsprocs/processes.rs
+++ src/fsprocs/processes.rs
@@ -35,7 +35,51 @@ mod proc2 {
 }
 
 // ---
+mod proc3 {
+    use super::*;
+
+    //
+    fn shell(cmd: &str) -> (String, bool) {
+        let cmd = format!("{} 2>&1", cmd);
+        let shell = if cfg!(windows) { "cmd.exe" } else { "/bin/sh" };
+        let flag = if cfg!(windows) { "/c" } else { "-c" };
+        let output = Command::new(shell)
+            .arg(flag)
+            .arg(&cmd)
+            .output()
+            .expect("no shell?");
+
+        (
+            String::from_utf8_lossy(&output.stdout)
+                .trim_end()
+                .to_string(),
+            output.status.success(),
+        )
+    }
+
+    // ---
+    fn shell_success(cmd: &str) -> Option<String> {
+        let (output, success) = shell(cmd);
+        if success {
+            Some(output)
+        } else {
+            None
+        }
+    }
+
+    //
+    pub fn proc3_examples() {
+        let cmd = "ls -lh";
+        match shell_success(cmd) {
+            Some(out) => println!("{}", out),
+            None => println!(""),
+        }
+    }
+}
+
+// ---
 pub fn processes_examples() {
     proc1::proc1_examples();
     proc2::proc2_examples();
+    proc3::proc3_examples();
 }
