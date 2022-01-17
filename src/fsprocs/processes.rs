48ad62cb4662decb135beb2d455bba11a30fd0bc
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-17 18:49:11 +0100

    Learn how to capture process output

--- src/fsprocs/processes.rs
+++ src/fsprocs/processes.rs
@@ -14,6 +14,28 @@ mod proc1 {
     }
 }
 
+// ---
+mod proc2 {
+    use super::*;
+
+    // ---
+    pub fn proc2_examples() {
+        let output = Command::new("rustc").arg("-V").output().expect("no rustc");
+
+        if output.status.success() {
+            println!("Ok!");
+        }
+
+        println!(
+            "len stdout {} stderr {}",
+            output.stdout.len(),
+            output.stderr.len()
+        );
+    }
+}
+
+// ---
 pub fn processes_examples() {
     proc1::proc1_examples();
+    proc2::proc2_examples();
 }
