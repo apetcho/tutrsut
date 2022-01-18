0f7fe996f0674a84220be0f6c791ebef1a57508a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-18 14:25:35 +0100

    Implement trait Quack on different animals object

--- src/oop/mod.rs
+++ src/oop/mod.rs
@@ -1,17 +1,22 @@
 //! Object-Orientation in Rust
+mod animals;
 mod traitobjs;
 
 mod ooptut {
     use super::*;
     pub fn ooptut_examples() {
+        println!("-------------------");
+        println!("  TRAITS OBJECTS   ");
+        println!("-------------------");
         traitobjs::traitobjs_examples();
+        println!("-------------");
+        println!("   ANIMALS   ");
+        println!("-------------");
+        animals::animals_examples();
     }
 }
 
 // ---
 pub fn oop_examples() {
-    println!("-------------------");
-    println!("  TRAITS OBJECTS   ");
-    println!("-------------------");
     ooptut::ooptut_examples();
 }
