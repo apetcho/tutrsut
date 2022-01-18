6ffdc97a80eef39f0597a5c602144264afcb9275
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-18 14:14:35 +0100

    Learn how to use smart point 'Box' in conjunction with trait objects

--- src/oop/traitobjs.rs
+++ src/oop/traitobjs.rs
@@ -26,4 +26,11 @@ pub fn traitobjs_examples() {
     for d in v.iter() {
         println!("show -> {}", d.show());
     }
+
+    let answer = Box::new(42);
+    let maybe_pi = Box::new(3.14);
+    let show_list: Vec<Box<dyn Show>> = vec![answer, maybe_pi];
+    for d in &show_list {
+        println!("show => {}", d.show());
+    }
 }
