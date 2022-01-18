359a3a5c812d7c5b69d0d364e65c824dc2d59b90
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-18 14:30:07 +0100

    Show how runtime dispatch can produce funny results

--- src/oop/animals.rs
+++ src/oop/animals.rs
@@ -26,12 +26,22 @@ impl Quack for RandomBird {
         }
     }
 }
-
+// ---
+impl Quack for i32 {
+    fn quack(&self) {
+        for i in 0..*self {
+            print!("quack {} ", i);
+        }
+        println!("");
+    }
+}
+// ---
 pub fn animals_examples() {
     let duck1 = Duck();
     let duck2 = RandomBird { is_a_parrot: false };
     let parrot = RandomBird { is_a_parrot: true };
-    let ducks: Vec<&dyn Quack> = vec![&duck1, &duck2, &parrot];
+    let n = 4;
+    let ducks: Vec<&dyn Quack> = vec![&duck1, &duck2, &parrot, &n];
     for d in &ducks {
         d.quack();
     }
