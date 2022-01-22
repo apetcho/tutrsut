988ee87edda40446f8e192bfa8d30fc5699b1711
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 12:46:50 +0100

    Learn about keyword 'loop' with return value for its body

--- src/keywords/kwtut.rs
+++ src/keywords/kwtut.rs
@@ -322,4 +322,13 @@ pub fn kw_loop() {
         i *= 2;
     }
     println!("i = {}", i);
+    //
+    i = 1;
+    let something = loop {
+        i *= 2;
+        if i > 100 {
+            break i;
+        }
+    };
+    println!("something = {}", something);
 }
